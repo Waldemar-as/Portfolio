b3e4b0d3be3a3799b1e5a6aa2b023d8ef0437712
Waldemar <waldemar.as@hotmail.de>
2022-09-13 15:53:21 +0200

    navbar erweitert.

---
+++ scirpt.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+    
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+    
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+    
+      $('.animated-icon3').toggleClass('open');
+    });});
+
+
+
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $("#mainNavbar");
+      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    })
+  })
